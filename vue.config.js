1c23721e13b6c423e1561fd272cb29b14d076894
muzishan <suhua.liu@yuemia.com>
2018-10-11 11:53:47 +0800

    test push mu

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,18 @@
-exports.modules = {};
+exports.modules = {
+  // baseUrl: "/",
+  // outputDir: "mobile",
+  // assetsDir: "FrontStatic",
+  // productionSourceMap: false,
+  // devServer: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:4000",
+  //       changeOrigin: true
+  //     },
+  //     "/static": {
+  //       target: "http://localhost:4000",
+  //       changeOrigin: true
+  //     }
+  //   }
+  // }
+};
