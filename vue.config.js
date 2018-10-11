a5864ae20b4509018fb33c2a9cb33f14d20c5698
muzishan <suhua.liu@yuemia.com>
2018-10-11 11:55:42 +0800

    v0.2

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 exports.modules = {
-  // baseUrl: "/",
-  // outputDir: "mobile",
-  // assetsDir: "FrontStatic",
-  // productionSourceMap: false,
-  // devServer: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:4000",
-  //       changeOrigin: true
-  //     },
-  //     "/static": {
-  //       target: "http://localhost:4000",
-  //       changeOrigin: true
-  //     }
-  //   }
-  // }
+  baseUrl: "/",
+  outputDir: "mobile",
+  assetsDir: "FrontStatic",
+  productionSourceMap: false,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: true
+      },
+      "/static": {
+        target: "http://localhost:4000",
+        changeOrigin: true
+      }
+    }
+  }
 };
